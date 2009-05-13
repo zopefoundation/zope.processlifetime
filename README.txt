90bc135b25be02d639879f88ac4e9fcd9dce5813
Tres Seaver <tseaver@palladion.com>
2009-05-13 19:02:48 +0000

    Port event interfaces / implementations from zope.app.appsetup.

---
+++ README.txt
@@ -0,0 +1,7 @@
+zope.processlifetime README
+===========================
+
+This package provides interfaces / implementations for events relative to
+the lifetime of a server process (startup, database opening, etc.)
+
+It is derived from Zope 3.4's 'zope.app.appsetup'.
---
+++ setup.py
@@ -0,0 +1,58 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.processlifetime package
+
+$Id$
+"""
+
+import os
+
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name='zope.processlifetime',
+    version = '1.0dev',
+    author='Zope Corporation and Contributors',
+    author_email='zope-dev@zope.org',
+    description="Zope process lifetime events",
+    long_description=(read('README.txt') + '\n\n' +
+                      read('CHANGES.txt')),
+    license='ZPL 2.1',
+    keywords="zope process lifetime events",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3',
+        'Framework :: Zope2',
+        ],
+    url='http://pypi.python.org/pypi/zope.processlifetime',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    test_suite='zope.processlifetime.tests',
+    namespace_packages=['zope'],
+    install_requires=['setuptools',
+                      'zope.interface',
+                     ],
+    include_package_data=True,
+    zip_safe=False,
+    )
