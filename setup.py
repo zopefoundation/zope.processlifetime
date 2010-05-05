32368d9a7984b554598f164efbecb35cd33fa07b
Tres Seaver <tseaver@palladion.com>
2010-05-05 19:13:12 +0000

    Conform to repository policy.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2009 Zope Corporation and Contributors.
+# Copyright (c) 2009 Zope Foundation and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -23,7 +23,7 @@ def read(*rnames):
 setup(
     name='zope.processlifetime',
     version = '1.0',
-    author='Zope Corporation and Contributors',
+    author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description="Zope process lifetime events",
     long_description=(read('README.txt') + '\n\n' +
