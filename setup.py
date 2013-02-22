d516d2317911daee07220245b583818f5b3625af
Stephan Richter <stephan.richter@gmail.com>
2013-02-22 13:04:27 -0500

    Fixed version number.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
 
 setup(
     name='zope.processlifetime',
-    version = '2.0',
+    version = '2.0.0.dev',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description="Zope process lifetime events",
