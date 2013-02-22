f3cf4eb7cf2e9c860a954921f5fb44110d6fbf40
Andrey Lebedev <andrey@lebedev.lt>
2013-02-22 09:37:53 +0000

    Releasing 2.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
 
 setup(
     name='zope.processlifetime',
-    version = '2.0dev',
+    version = '2.0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description="Zope process lifetime events",
