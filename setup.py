8a5be2e467f8e40203cfd1dc804877efe264ae07
Michael Howitz <mh@gocept.com>
2022-08-26 08:03:05 +0200

    Back to development: 2.5

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def read(*rnames):
 
 setup(
     name='zope.processlifetime',
-    version='2.4',
+    version='2.5.dev0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description="Zope process lifetime events",
