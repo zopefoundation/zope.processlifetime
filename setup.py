687efe47f7b3ffa06e54c507d2490c1770c2df28
Michael Howitz <icemac@gmx.net>
2025-09-12 09:33:37 +0200

    Back to development: 4.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*rnames):
 
 setup(
     name='zope.processlifetime',
-    version='4.0',
+    version='4.1.dev0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.dev',
     description="Zope process lifetime events",
